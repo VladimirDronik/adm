4fe15ac332bf526b150d631fae891c72b81c48e4
gittadm <vb2freelance@yandex.ru>
2019-09-16 15:21:41 +0300

    VPN modal

---
+++ resources/views/components/confirm_modal.blade.php
@@ -0,0 +1,16 @@
+<div id="confirm_modal" class="modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirm_modal_title">Подтверждение</h5>
+            </div>
+            <div class="modal-body text-left" id="confirm_modal_body" style="font-size: larger;"></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" id="confirm_modal_btn"></button>
+                <button type="button" class="btn btn-default" id="confirm_cancel_btn" data-dismiss="modal">Отмена</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<button type="button" id="confirm_init_btn" style="display: none;" data-toggle="modal" data-target="#confirm_modal">&nbsp;</button>
--- resources/views/network/edit.blade.php
+++ resources/views/network/edit.blade.php
@@ -13,7 +13,7 @@
                     @if(!isset($main_network) || !isset($network) || !isset($vpn))
                         <p class="text-danger">Не удалось загрузить данные</p>
                     @else
-                    {!! Form::model((object)[], ['route' => ['network.update'], 'method' => 'put', 'class' => 'form-horizontal form-bordered']) !!}
+                    {!! Form::model((object)[], ['route' => ['network.update'], 'method' => 'put', 'class' => 'form-horizontal form-bordered', 'id' => 'form']) !!}
                     {{ csrf_field() }}
                     <div class="form-body">
                         {{ Form::bs_alert() }}
@@ -41,4 +41,78 @@
             </div>
         </div>
     </div>
+    @include('components.confirm_modal')
+@endsection
+
+@section('scripts')
+    <script>
+        let data = {};
+        data.main_ip = '{{ $main_network[0] }}';
+        data.main_mask = '{{ $main_network[1] }}';
+        data.main_gateway = '{{ $main_network[2] }}';
+        data.ip = '{{ $network[0] }}';
+        data.mask = '{{ $network[1] }}';
+        data.vpn_address = '{{ $vpn[0] }}';
+        data.vpn_login = '{{ $vpn[1] }}';
+        data.vpn_password = '{{ $vpn[2] }}';
+
+        $(document).ready(function(){
+
+            function getChanges() {
+                let html = '';
+                let main_ip = $('input[name=main_ip]').val().trim();
+                let main_mask = $('input[name=main_mask]').val().trim();
+                let main_gateway = $('input[name=main_gateway]').val().trim();
+                let ip = $('input[name=ip]').val().trim();
+                let mask = $('input[name=mask]').val().trim();
+                let vpn_address = $('input[name=vpn_address]').val().trim();
+                let vpn_login = $('input[name=vpn_login]').val().trim();
+                let vpn_password = $('input[name=vpn_password]').val().trim();
+
+                if (main_ip !== '' && data.main_ip !== main_ip) {
+                   html += 'IP основного адреса изменен с '+data.main_ip+' на '+main_ip+'<br>';
+                }
+                if (main_mask !== '' && data.main_mask !== main_mask) {
+                    html += 'Маска основного адреса изменена с '+data.main_mask+' на '+main_mask+'<br>';
+                }
+                if (main_gateway !== '' && data.main_gateway !== main_gateway) {
+                    html += 'Шлюз основного адреса изменен с '+data.main_gateway+' на '+main_gateway+'<br>';
+                }
+                if (ip !== '' && data.ip !== ip) {
+                    html += 'IP адреса для подсети изменен с '+data.ip+' на '+ip+'<br>';
+                }
+                if (mask !== '' && data.mask !== mask) {
+                    html += 'Маска адреса для подсети изменена с '+data.mask+' на '+mask+'<br>';
+                }
+                if (vpn_address !== '' && data.vpn_address !== vpn_address) {
+                    html += 'Адрес сервера VPN изменен с '+data.vpn_address+' на '+vpn_address+'<br>';
+                }
+                if (vpn_login !== '' && data.vpn_login !== vpn_login) {
+                    html += 'Логин VPN изменен с '+data.vpn_login+' на '+vpn_login+'<br>';
+                }
+                if (vpn_password !== '' && data.vpn_password !== vpn_password) {
+                    html += 'Пароль VPN изменен с '+data.vpn_password+' на '+vpn_password+'<br>';
+                }
+                return html;
+            }
+
+            function showConfirmModal() {
+                let html = getChanges();
+                html += '<b>Устройство будет перезагружено и доступно по адресу '
+                    +$('input[name=main_ip]').val()+'</b>';
+                $('#confirm_modal_body').html(html);
+                $('#confirm_modal_btn').text('Перезагрузить');
+                $('#confirm_init_btn').click();
+            }
+
+            $('button[type=submit]').click(function(){
+                showConfirmModal();
+                return false;
+            });
+
+            $('#confirm_modal_btn').click(function(){
+                $('#form').submit();
+            });
+        });
+    </script>
 @endsection
