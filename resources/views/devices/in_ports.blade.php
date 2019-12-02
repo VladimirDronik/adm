b7cefef0204d77352d3b739d4d50f8c3d690bf21
gittadm <vb2freelance@yandex.ru>
2019-12-02 16:52:51 +0300

    ports table

--- resources/views/devices/in_ports.blade.php
+++ resources/views/devices/in_ports.blade.php
@@ -5,10 +5,9 @@
                 <th>#</th>
                 <th>Тип</th>
                 <th>Описание</th>
-                <th>Связанный объект</th>
-                <th>Действие</th>
-                <th class="text-center">Длит. нажатие</th>
-                <th class="text-center">Двойн. нажатие</th>
+                <th>Один. нажатие</th>
+                <th>Двойн. нажатие</th>
+                <th class="text-left">Длит. нажатие</th>
             </tr>
         </thead>
         <tbody>
@@ -18,9 +17,7 @@
             @php $port_count++; @endphp
             <tr>
                 <th scope="row"> {{ $port->num_port }}</th>
-                <td>
-                    <span class="badge badge-success">{{ $port->status }}</span>
-                </td>
+                <td><span class="badge badge-success">{{ $port->status }}</span></td>
                 <td>
                     <a href="#" data-toggle="modal" data-target="#name_modal"
                        id="name_port_{{ $port->id }}" onclick="getPortComment('{{ $port->id }}');">
@@ -31,31 +28,7 @@
                         @endif
                     </a>
                 </td>
-                <td>
-                    @if($port->eobject)
-                        <button type="button" class="btn btn-warning m-b-10 btn-sm"
-                                name="object" id="portobj_{{ $port->id }}"
-                                data-toggle="modal" data-target="#objectsModal"
-                                value="{{ $port->object}},{{$port->eobject->name}},portobj_{{ $port->id }}">
-                            <b>{{ optional($port->eobject)->name }}</b>
-                        </button>
-                    @else
-                        <button type="button" class="btn btn-default m-b-10 btn-sm"
-                                name="object" id="portobjempty_{{ $port->id }}"
-                                data-toggle="modal" data-target="#objectsModal"
-                                value="empty,empty,portobjempty_{{ $port->id }}">
-                            Отсутствует
-                        </button>
-                    @endif
-                </td>
-                <td>
-                    {{--                                    @if($port->eobject && $port->status !== 'out')--}}
-                    {{--                                        <button type="button" id="method_btn_{{ $port->id }}" class="btn btn-warning m-b-10 btn-sm" data-toggle="modal" data-target="#actionModal" onclick="click_port_method('method', {{ $port->id }}, '{{ optional($port->eobject)->name }}');">--}}
-                    {{--                                            @if($port->method) <b>Метод: {{ optional($port->emethod)->name }}</b> @else <b class="text-danger">Метод не выбран</b> @endif--}}
-                    {{--                                        </button>--}}
-                    {{--                                    @elseif($port->status !== 'out')--}}
-                    {{--                                        <button type="button" id="method_btn_{{ $port->id }}" class="btn btn-default m-b-10 btn-sm" data-toggle="modal" data-target="#actionModal" onclick="click_port_method('none', {{ $port->id }}, 'none');">Отсутствует</button>--}}
-                    {{--                                    @endif--}}
+                <td class="text-left">
                     @if($port->eobject)
                         <button type="button" id="viewmethod_{{ $port->id }}"
                                 name="method" class="btn btn-warning m-b-10 btn-sm" data-toggle="modal"
@@ -71,25 +44,52 @@
                             Отсутствует</button>
                     @endif
                 </td>
-                <td class="text-center">
-                    <input type="checkbox" class="long_checkbox" data-id="{{ $port->id }}" style="cursor: pointer;" autocomplete="off" value="1" @if($port->longclick) checked @endif></td>
-                <td class="text-center">
-                    <input type="checkbox" class="double_checkbox" data-id="{{ $port->id }}" style="cursor: pointer;" autocomplete="off" value="1" @if($port->doubleclick) checked @endif> </td>
+                <td class="text-left">
+                    @if($port->eobject)
+                        <button type="button" id="viewmethod_{{ $port->id }}"
+                                name="method" class="btn btn-warning m-b-10 btn-sm" data-toggle="modal"
+                                value="{{ $port->method}},{{optional($port->emethod)->name}},viewmethod_{{ $port->id }}"
+                                data-target="#methodsModal">
+                            @if($port->method)<b>Метод: {{ optional($port->emethod)->name }}</b>@else <b class="text-danger">Метод не выбран</b> @endif
+                        </button>
+                    @else
+                        <button type="button" id="viewmethodempty_{{ $port->id }}"
+                                name="method" class="btn btn-default m-b-10 btn-sm" data-toggle="modal"
+                                value="empty,empty,viewmethodempty_{{ $port->id }}"
+                                data-target="#methodsModal">
+                            Отсутствует</button>
+                    @endif
+                </td>
+                <td class="text-left">
+                    @if($port->eobject)
+                        <button type="button" id="viewmethod_{{ $port->id }}"
+                                name="method" class="btn btn-warning m-b-10 btn-sm" data-toggle="modal"
+                                value="{{ $port->method}},{{optional($port->emethod)->name}},viewmethod_{{ $port->id }}"
+                                data-target="#methodsModal">
+                            @if($port->method)<b>Метод: {{ optional($port->emethod)->name }}</b>@else <b class="text-danger">Метод не выбран</b> @endif
+                        </button>
+                    @else
+                        <button type="button" id="viewmethodempty_{{ $port->id }}"
+                                name="method" class="btn btn-default m-b-10 btn-sm" data-toggle="modal"
+                                value="empty,empty,viewmethodempty_{{ $port->id }}"
+                                data-target="#methodsModal">
+                            Отсутствует</button>
+                    @endif
+                </td>
             </tr>
             @endif
         @endforeach
         </tbody>
         @if($port_count > 10)
             <tfoot>
-            <tr>
-                <th>#</th>
-                <th>Тип</th>
-                <th>Описание</th>
-                <th>Связанный объект</th>
-                <th>Действие</th>
-                <th class="text-center">Длит. нажатие</th>
-                <th class="text-center">Двойн. нажатие</th>
-            </tr>
+                <tr>
+                    <th>#</th>
+                    <th>Тип</th>
+                    <th>Описание</th>
+                    <th>Один. нажатие</th>
+                    <th>Двойн. нажатие</th>
+                    <th class="text-left">Длит. нажатие</th>
+                </tr>
             </tfoot>
         @endif
     </table>
