e42e799261da398f5b37335ef9f267b7710732d9
gittadm <vb2freelance@yandex.ru>
2019-12-02 22:13:15 +0300

    generate fake button fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer"> © 2019 All rights reserved. TouchOn Technology inc. To more info visit:
     <a href="https://touchon.tech" target="_blank">touchon.tech</a>
     @if(\Illuminate\Support\Facades\App::environment('local'))
-        <a href="{{ route('generate.fake') }}" class="btn btn-outline-info pull-right" title="Сброс бд: заново выполнение миграций и заполнение тестовыми данными">Сгененировать тестовые данные</a>
+        <a href="{{ route('generate.fake') }}" class="btn btn-outline-info pull-right" title="Сброс бд: заново выполнение миграций и заполнение тестовыми данными">Сгенерировать тестовые данные</a>
     @endif
 </footer>
 <!-- End footer -->
