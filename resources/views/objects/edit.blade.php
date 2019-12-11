b2f8c7252fbf85333d31b32b9f9f345b70b92d98
gittadm <vb2freelance@yandex.ru>
2019-12-11 15:21:11 +0300

    termostats: auto object, methods, events fix

--- resources/views/objects/edit.blade.php
+++ resources/views/objects/edit.blade.php
@@ -121,16 +121,16 @@
                         {{ Form::bs_title('Cобытия объекта') }}
                         @if(count($object->scheduler_tasks))
                             <div class="form-group row">
-                                <label class="col-md-3"><i>Событие</i></label>
-                                <div class="col-md-3"><i>Метод</i></div>
+                                <label class="col-md-5"><i>Событие</i></label>
+                                <div class="col-md-5"><i>Метод</i></div>
                             </div>
                             <div id="events_div">
                                 @foreach($object->scheduler_tasks as $scheduler_task)
                                     <div class="form-group row" id="ediv{{$scheduler_task->method}}">
-                                        <label class="col-md-3">
+                                        <label class="col-md-5">
                                             <a href="{{ route('events.edit', [$scheduler_task->id]) }}">{{ $scheduler_task->name }}</a>
                                         </label>
-                                        <div class="col-md-3">
+                                        <div class="col-md-5">
                                             {{ optional($scheduler_task->emethod)->name }}
                                         </div>
                                     </div>
