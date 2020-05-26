b0a18a6d4ea2364dfd9e9637661e8e9c79b6f349
VladimirDronik <kinord@yandex.ru>
2020-05-26 15:51:08 +0300

    messages migration bugfix

--- database/migrations/2020_05_25_061038_create_messages_table.php
+++ database/migrations/2020_05_25_061038_create_messages_table.php
@@ -13,7 +13,7 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('text')->nullable();
             $table->tinyInteger('priority')->nullable();
