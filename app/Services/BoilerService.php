52597fe1828d62a69479401a54c9b89bfc76c368
VladimirDronik <kinord@yandex.ru>
2021-04-15 15:58:23 +0300

    adding boiler bugfix

--- app/Services/BoilerService.php
+++ app/Services/BoilerService.php
@@ -56,7 +56,7 @@ class BoilerService
         $boiler->ip_address = $data['ip_address_boiler'];
         $boiler->model = $data['type_boiler'];
         $boiler->mode = 'auto';
-        $boiler->active = 1;
+       
 
         DB::transaction(function () use (&$boiler, $data) {
 
