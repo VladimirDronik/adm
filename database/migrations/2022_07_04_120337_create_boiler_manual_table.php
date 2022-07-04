9decbecaf61b0ad8dfdcce155a3a40fd5af87b40
Pavel Solin <ps@stroytelecom.com>
2022-07-04 14:11:09 +0300

    Add foreign key in migrations boiler_manual, boiler_water

--- database/migrations/2022_07_04_120337_create_boiler_manual_table.php
+++ database/migrations/2022_07_04_120337_create_boiler_manual_table.php
@@ -15,10 +15,12 @@ class CreateBoilerManualTable extends Migration
     {
         Schema::create('boiler_manual', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('id_object');
             $table->float('set_value')->nullable()->default(NULL);
             $table->float('min_value')->nullable()->default(NULL);
             $table->float('max_value')->nullable()->default(NULL);
+
+            $table->foreign('id_object')->references('id')->on('objects')
+                  ->onUpdate('cascade')->onDelete('set null');
         });
     }
 
