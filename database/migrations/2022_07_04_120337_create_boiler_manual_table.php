c553abc1ef9d3dcaa1e075b8f1b7c49e7612070c
Pavel Solin <ps@stroytelecom.com>
2022-07-04 13:39:44 +0300

    Add migrations for boiler, boiler_manual, boiler_water, internal_pages and elements

---
+++ database/migrations/2022_07_04_120337_create_boiler_manual_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoilerManualTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boiler_manual', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_object');
+            $table->float('set_value')->nullable()->default(NULL);
+            $table->float('min_value')->nullable()->default(NULL);
+            $table->float('max_value')->nullable()->default(NULL);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boiler_manual');
+    }
+}
