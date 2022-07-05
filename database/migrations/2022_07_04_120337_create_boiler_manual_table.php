514150b7fa844a616f5489fd549cd39f48d9ec7b
Pavel Solin <ps@stroytelecom.com>
2022-07-05 15:18:02 +0300

    Bugfix: migration - key column doesn't exist

--- database/migrations/2022_07_04_120337_create_boiler_manual_table.php
+++ database/migrations/2022_07_04_120337_create_boiler_manual_table.php
@@ -16,6 +16,7 @@ class CreateBoilerManualTable extends Migration
         if (!Schema::hasTable('boiler_manual')) {
             Schema::create('boiler_manual', function (Blueprint $table) {
                 $table->increments('id');
+                $table->integer('id_object')->unsigned();
                 $table->float('set_value')->nullable()->default(NULL);
                 $table->float('min_value')->nullable()->default(NULL);
                 $table->float('max_value')->nullable()->default(NULL);
