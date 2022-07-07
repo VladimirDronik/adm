085db4c3ab48f375bf0a29a5fa7fa70f45ad95c0
Pavel Solin <ps@stroytelecom.com>
2022-07-07 12:48:37 +0300

    Add models: BoilerManual, BoilerWater, InternalPage

---
+++ app/Models/InternalPage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\InternalPage
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device query()
+ * @mixin \Eloquent
+ *
+ * @property int    $id
+ * @property int    idElement
+ * @property string type
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device whereIpAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Device whereType($value)
+ *
+ * @property-read \App\Models\Elements $element
+ */
+class InternalPage extends Model
+{
+    public    $timestamps = false;
+    protected $guarded    = ['id'];
+
+    /* relations */
+    public function element()
+    {
+        return $this->belongsTo(Elements::class, 'idElement');
+    }
+}
