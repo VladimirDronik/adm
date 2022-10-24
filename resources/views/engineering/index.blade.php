71dafa9653143c2e27ef35f7702787b5e70f7206
s044316 <kinordikus@gmail.com>
2022-10-24 19:06:19 +0300

    adding idObject fot new eng object

--- resources/views/engineering/index.blade.php
+++ resources/views/engineering/index.blade.php
@@ -100,7 +100,7 @@
         let del_id;
 
  @if(!empty(Session::get('success')) && Session::get('success') == 'Котёл успешно добавлен')
-            {{ session('success') }}
+
 
               $('#modalNewMenu').show();
               $('#modal_newmenu_init_btn').click();
