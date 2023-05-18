2b4715559a3c2dca2d0b2e2de0cc5c03bcc064e5
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2023-05-18 22:05:43 +0300

    add Добавил миграции меняющие тип данных столбцов в таблицах boiler_manual и boiler_water

---
+++ database/migrations/2023_05_18_220233_change_min_and_max_value_in_boiler_boiler_manual_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeMinAndMaxValueInBoilerBoilerManualTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('boiler_manual', function (Blueprint $table) {
+            $table->integer('min_value')->nullable()->default(null)->change();
+            $table->integer('max_value')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('boiler_manual', function (Blueprint $table) {
+            $table->float('min_value')->nullable()->default(null)->change();
+            $table->float('max_value')->nullable()->default(null)->change();
+        });
+    }
+}
