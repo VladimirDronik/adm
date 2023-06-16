c7e3e376851278120c309ec7b4fdadb28af16ca6
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2023-06-16 19:20:04 +0300

    add Добавил миграцию создания таблицы boiler_auto

---
+++ database/migrations/2023_06_16_191445_create_boiler_auto_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoilerAutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boiler_auto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('t_out');
+            $table->integer('t_water');
+            $table->unsignedInteger('id_object');
+
+            $table->foreign('id_object')
+                ->references('id')
+                ->on('objects')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boiler_auto');
+    }
+}
