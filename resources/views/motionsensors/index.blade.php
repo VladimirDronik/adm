200c2b6bca089616314d6d2330866bbcbf52c6e8
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2023-07-12 19:50:22 +0300

    ref Убрал объекты из списков датчиков

--- resources/views/motionsensors/index.blade.php
+++ resources/views/motionsensors/index.blade.php
@@ -36,12 +36,8 @@
                         <table class="table">
                             <thead>
                                 <tr>
-
-                                    <th style="width: 60px;">ID</th>
+                                    <th>ID</th>
                                     <th>Название</th>
-                                    @can('devices.show-object')
-                                        <th>Объект</th>
-                                    @endcan
                                     <th style="width: 60px;"></th>
                                     <th style="width: 160px;"></th>
                                 </tr>
@@ -51,14 +47,6 @@
                                     <tr id="tr{{$motionsensor->id}}">
                                         <td scope="row">{{ $motionsensor->iobject['id'] }}</td>
                                         <td><a href="{{ route('motionsensors.edit', [$motionsensor->id]) }}">{{ $motionsensor->name }}</a></td>
-                                        @can('devices.show-object')
-                                            <td>@if($motionsensor->object)
-                                                    <a href="{{ route('objects.edit', [$motionsensor->id_object]) }}">{{ optional($motionsensor->object)->name }}</a>
-                                                @else
-                                                    Не указан
-                                                @endif
-                                            </td>
-                                        @endcan
                                         <td align="center" class="text-center">
                                             <a href="{{ route('motionsensors.edit', [$motionsensor->id]) }}" class="btn btn-info btn-sm btn-rounded">
                                                 <i class="fa fa-cog fa-lg"></i>
@@ -75,12 +63,8 @@
                             </tbody>
                             <tfoot>
                                 <tr>
-
-                                    <th style="width: 60px;">ID</th>
+                                    <th>ID</th>
                                     <th>Название</th>
-                                    @can('devices.show-object')
-                                        <th>Объект</th>
-                                    @endcan
                                     <th style="width: 60px;"></th>
                                     <th style="width: 60px;"></th>
                                 </tr>
