7e19ea33b1a715dd4d69f4c81eddb0ea8c2d42e8
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2023-07-12 17:40:49 +0300

    ref Убрал объект со страниц редактирования устройств

--- resources/views/lamps/edit_tabs/main.blade.php
+++ resources/views/lamps/edit_tabs/main.blade.php
@@ -14,24 +14,7 @@
 
 {{ Form::bs_text('name', 'Название*:', null, ['required' => true]) }}
 
-@if(($lamp->object && $lamp->object->is_system) || !$can['devices.show-object'])
-    <div class="form-group row">
-        <label class="control-label text-right col-md-3 label-fix" for="">
-            Объект:     </label>
-        <div class="col-md-9">
-            <div class="mt-2">
-                <a class="a-color" href="{{ route('objects.edit', [$lamp->id_object]) }}">
-                    {{ $lamp->object->name }}
-                    @if($lamp->object && $lamp->object->is_system) (системный) @endif</a>
-            </div>
-        </div>
-    </div>
-    <input type="hidden" name="id_object" value="{{ $lamp->id_object }}">
-    @else
-    {{ Form::bs_autoselect_and_btn('id_object', 'Объект*:', $objects, old('id_object', $lamp->id_object), false, false, ['required' => true]) }}
-    @endif
-
-
+<input type="hidden" name="id_object" value="{{ $lamp->id_object }}">
 
     <div class="col-sm-12 pr-0 mt-4">
         {{ Form::bs_autoselect('device_id', 'Контроллер:', $devices, old('device_id', $idDevice),
