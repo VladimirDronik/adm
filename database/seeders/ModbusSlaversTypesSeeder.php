130b5dd58a40c6744c681fe28df769c398238e85
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2024-03-15 15:27:06 +0300

    add Добавил новые поля таблице modbus_slavers_types

--- database/seeders/ModbusSlaversTypesSeeder.php
+++ database/seeders/ModbusSlaversTypesSeeder.php
@@ -13,26 +13,38 @@ class ModbusSlaversTypesSeeder extends Seeder
             [
                 'type' => 'wb-led',
                 'name' => 'WB-LED',
+                'purpose' => 'light',
+                'relay' => 0,
             ],
             [
                 'type' => 'wb-mir',
                 'name' => 'WB-MIR',
+                'purpose' => 'ir',
+                'relay' => 0,
             ],
             [
                 'type' => 'wb-map12e',
                 'name' => 'WB-MAP12E',
+                'purpose' => 'meter',
+                'relay' => 0,
             ],
             [
                 'type' => 'wb-mrm2-mini',
                 'name' => 'WB-MRM2-mini',
+                'purpose' => 'relay',
+                'relay' => 1,
             ],
             [
                 'type' => 'bcg-301-w',
                 'name' => 'Nevoton OPENTHERM',
+                'purpose' => 'heat',
+                'relay' => 0,
             ],
             [
                 'type' => 'ecodim-dali-gw2',
                 'name' => 'Ecodim DALI GW2',
+                'purpose' => 'light',
+                'relay' => 1,
             ]
         ];
     }
@@ -42,13 +54,13 @@ class ModbusSlaversTypesSeeder extends Seeder
      */
     public function run(): void
     {
-        $types = $this->getTypes();
+        $slaverTypes = $this->getTypes();
 
-        foreach ($types as $type) {
-            ModbusSlaversType::updateOrCreate(
-                ['type' => $type['type']],
-                ['name' => $type['name']]
-            );
+        foreach ($slaverTypes as $slaverType) {
+            $type = $slaverType['type'];
+            unset($slaverType['type']);
+
+            ModbusSlaversType::updateOrCreate(['type' => $type], $slaverType);
         }
     }
 }
