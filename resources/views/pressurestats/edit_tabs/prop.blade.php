0b71f8b13b2b4592339f6241035bea2ee63c0487
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2024-04-10 11:56:25 +0300

    fix Исправил опечатку на странице редактирования датчика давления

--- resources/views/pressurestats/edit_tabs/prop.blade.php
+++ resources/views/pressurestats/edit_tabs/prop.blade.php
@@ -4,8 +4,8 @@
 {{ Form::bs_number('optimal', 'Оптимальное давление*:', old('optimal', $pressurestat->optimal), ['min' => 0, 'max' => 2000, 'required' => true], 'Давление, которое должно быть в помещении') }}
 {{ Form::bs_number('gisteresis', 'Гистерезис*:', old('gisteresis', $pressurestat->gisteresis), ['min' => 0, 'max' => 100, 'required' => true]) }}
 
-{{ Form::bs_simple_text('Минимальная освещенность:', $pressurestat->min_threshold) }}
-{{ Form::bs_simple_text('Максимальная освещенность:', $pressurestat->max_threshold) }}
+{{ Form::bs_simple_text('Минимальное давление:', $pressurestat->min_threshold) }}
+{{ Form::bs_simple_text('Максимальное давление:', $pressurestat->max_threshold) }}
 
 {{ Form::bs_number('min_alarm', 'Мин. аварийное давление*:', old('min_alarm', $pressurestat->min_alarm), ['min' => 0, 'max' => 10000, 'required' => true], '') }}
 {{ Form::bs_number('max_alarm', 'Макс. аварийное давление*:', old('max_alarm', $pressurestat->max_alarm), ['min' => 0, 'max' => 10000, 'required' => true], '') }}
