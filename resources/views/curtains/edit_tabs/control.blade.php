e2e540a4b32fcd0f956217fbe382beb332cdee34
Artem Seredyuk <1seredyuk.artem1@gmail.com>
2024-10-25 14:03:07 +0300

    ref Убрал проверки поля Процент открытия у штор

--- resources/views/curtains/edit_tabs/control.blade.php
+++ resources/views/curtains/edit_tabs/control.blade.php
@@ -13,7 +13,7 @@
     <div class="form-group row">
         <label class="control-label text-right col-md-3 label-fix" for="percent">Процент открытия:</label>
         <div class="col-md-2">
-            <input class="form-control" autocomplete="off" min="0" max="100" name="percent" type="number" value="{{ old('percent', $curtain->percent) }}">
+            <input class="form-control" autocomplete="off" name="percent" value="{{ old('percent', $curtain->percent) }}">
         </div>
         <button type="button" class="btn btn-success m-b-10 m-l-5" id="setPercentBtn">Задать</button>
     </div>
